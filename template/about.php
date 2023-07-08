8a073bedfd6e6fe412878d096836e75ed4a9d3a0
KhemJiAimaim <119541041+KhemJiAimaim@users.noreply.github.com>
2023-07-08 14:27:58 +0700

    font and everyting

--- template/about.php
+++ template/about.php
@@ -1,4 +1,5 @@
 <div class="about-content">
+    <div class="about-content-shadow">
     <div class="head-text">
         <h1>เกี่ยวกับเรา</h1>
     </div>
@@ -14,12 +15,10 @@
         <p class="about-text-title">เบอร์ฟอร์ยู</p>
         <ol class="about-text-doc">
             <li>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas officiis repudiandae dolores
-                    vitae, ipsam laboriosam blanditiis quis molestias accusantium amet?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellat, porro libero autem, ipsa sit commodi impedit, eaque explicabo facilis quae facere at soluta voluptate! Earum qui quia deserunt itaque reprehenderit sed fuga velit doloremque aliquid quae dolorem, recusandae esse unde ipsam sint consequatur sequi excepturi at nobis vitae error modi accusamus labore? Optio sit, tempora consequatur voluptate consequuntur ipsam? Molestias iure vel repellat unde ex doloremque sapiente? Fugiat eum expedita libero unde magni quos iusto pariatur quibusdam dolor. Corporis eveniet quaerat cum cumque culpa molestias pariatur ea soluta hic et quasi sint, aut nihil ab sequi. Consequatur, voluptas doloremque!</p>
             </li>
             <li>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas officiis repudiandae dolores
-                    vitae, ipsam laboriosam blanditiis quis molestias accusantium amet?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat totam laboriosam natus ratione eius. Error nihil, deserunt quia impedit, expedita doloremque corporis non quisquam officia accusantium ut pariatur cum? Consequatur quos necessitatibus minus odio delectus, dignissimos soluta expedita nemo odit numquam qui hic ab a sint nulla temporibus. Quidem, esse.</p>
             </li>
             <figure class="dbd-logo">
                 <img src="img/logo/Rectangle 898.png" alt="">
@@ -27,12 +26,10 @@
 
 
             <li>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas officiis repudiandae dolores
-                    vitae, ipsam laboriosam blanditiis quis molestias accusantium amet?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellat, porro libero autem, ipsa sit commodi impedit, eaque explicabo facilis quae facere at soluta voluptate! Earum qui quia deserunt itaque reprehenderit sed fuga velit doloremque aliquid quae dolorem, recusandae esse unde ipsam sint consequatur sequi excepturi at nobis vitae error modi accusamus labore? Optio sit, tempora consequatur voluptate consequuntur ipsam? Molestias iure vel repellat unde ex doloremque sapiente? Fugiat eum expedita libero unde magni quos iusto pariatur quibusdam dolor. Corporis eveniet quaerat cum cumque culpa molestias pariatur ea soluta hic et quasi sint, aut nihil ab sequi. Consequatur, voluptas doloremque!</p>
             </li>
             <li>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas officiis repudiandae dolores
-                    vitae, ipsam laboriosam blanditiis quis molestias accusantium amet?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat totam laboriosam natus ratione eius. Error nihil, deserunt quia impedit, expedita doloremque corporis non quisquam officia accusantium ut pariatur cum? Consequatur quos necessitatibus minus odio delectus, dignissimos soluta expedita nemo odit numquam qui hic ab a sint nulla temporibus. Quidem, esse.</p>
             </li>
         </ol>
 
@@ -40,5 +37,6 @@
     <!-- แผนที่ -->
     <div class="map" style="padding:15px;">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.562748801084!2d100.67860831527442!3d13.805212199771887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d63be66849815%3A0x4a19378f6e81e6f7!2z4LmA4Lia4Lit4Lij4LmM4LmA4LiU4LmH4LiU4LmA4Lia4Lit4Lij4LmM4Lij4Lix4Lia4LmC4LiK4LiELmNvbQ!5e0!3m2!1sth!2sth!4v1583224259129!5m2!1sth!2sth" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    </div>
     </div>
 </div>
